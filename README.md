7e32538f98627ceaaadb73263cc6b0fba0b6130a
LCQDev <lcquinhone@outlook.com.br>
2023-04-26 16:24:05 -0400

    Base Criada

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # nodets-pet
-Projeto curso B7Web
+Projeto feito no módul do curso Node + Typescript - B7Web
+
+### Pré-requisitos globais:
+`npm install -g nodemon typescript ts-node`
+
+### Instalação
+`npm install`
+
+### para rodar o projeto
+`npm run start-dev`
---
+++ src/server.ts
@@ -0,0 +1,27 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import mustache from 'mustache-express'
+import path from 'path'
+import routes from './routes/index'
+
+dotenv.config()
+
+const server = express()
+
+// Configurações do template
+server.set('view engine', 'mustache')
+server.set('views', path.join(__dirname, 'views'))
+server.engine('mustache', mustache())
+
+// Configuração da pasta para arquivos static
+server.use(express.static(path.join(__dirname, '../public')))
+
+// Rotas
+server.use(routes)
+
+server.use((req, res) => {
+    res.send('Página não encontrada')
+})
+
+server.listen(process.env.PORT)
+
