c4d89fd9b4064318f38e56112ee567704132f178
LCQDev <lcquinhone@outlook.com.br>
2023-04-26 21:03:34 -0400

    Atualização final

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -4,9 +4,17 @@ import { createMenuObj } from '../helpers/createMenuObj'
 import { Pet } from '../models/Pet'
 
 export const search = (req:Request, res:Response) => {
+
+    let query: string = req.query.q as string
+
+    if(!query){
+        res.redirect('/')
+        return
+    }
+
     res.render('pages/page', {
         menu: createMenuObj(''),
         list: Pet.getFromName(req.query.q as string),
-        query: req.query.q as string
+        query
     })
 }
