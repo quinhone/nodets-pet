feeb14c0c2378a7869157ae0cdd3d606298b5940
LCQDev <lcquinhone@outlook.com.br>
2023-04-26 20:30:28 -0400

    Projeto Final1

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,17 +1,47 @@
 import { Request, Response } from "express";
+import { createMenuObj } from '../helpers/createMenuObj'
+import { Pet } from '../models/Pet'
 
 export const home = (req:Request, res:Response) => {
-    //res.render('pages/page')
+    res.render('pages/page', {
+        menu: createMenuObj('all'),
+        banner: {
+            title: 'Todos os animais',
+            background: 'allanimals.jpg'
+        },
+        list: Pet.getAll()
+    })
 }
 
 export const dogs = (req:Request, res:Response) => {
-    //res.render('pages/page')
+    res.render('pages/page', {
+        menu: createMenuObj('dog'),
+        banner: {
+            title: 'Cachorros',
+            background: 'banner_dog.jpg'
+        },
+        list: Pet.getFromType('dog')
+    })
 }
 
 export const cats = (req:Request, res:Response) => {
-    //res.render('pages/page')
+    res.render('pages/page', {
+        menu: createMenuObj('cat'),
+        banner: {
+            title: 'Gatos',
+            background: 'banner_cat.jpg'
+        },
+        list: Pet.getFromType('cat')
+    })
 }
 
 export const fishes = (req:Request, res:Response) => {
-    //res.render('pages/page')
+    res.render('pages/page', {
+        menu: createMenuObj('fish'),
+        banner: {
+            title: 'Peixes',
+            background: 'banner_fish.jpg'
+        },
+        list: Pet.getFromType('fish')
+    })
 }
--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,5 +1,12 @@
 import { Request, Response } from "express";
 
+import { createMenuObj } from '../helpers/createMenuObj'
+import { Pet } from '../models/Pet'
+
 export const search = (req:Request, res:Response) => {
-    //res.render('pages/page')
+    res.render('pages/page', {
+        menu: createMenuObj(''),
+        list: Pet.getFromName(req.query.q as string),
+        query: req.query.q as string
+    })
 }
